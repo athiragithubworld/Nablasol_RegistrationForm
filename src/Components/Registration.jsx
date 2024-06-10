c2cb4379a42a348b1e9f4b1db5683933185ce73d
athiragithubworld <athirahrms@gmail.com>
2024-06-10 11:32:37 +0530

    Updates

--- src/Components/Registration.jsx
+++ src/Components/Registration.jsx
@@ -4,7 +4,10 @@ import BusinessForm from "./BusinessForm";
 import StepIndicator from "./Pagination/StepIndicator";
 
 const Registration = () => {
+  // State to manage the current step in the registration process
   const [step, setStep] = useState(1);
+
+  // State to manage the form data
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
@@ -19,30 +22,35 @@ const Registration = () => {
     taxId: "",
     brandType: "local",
   });
+
+  // State to manage the validity of the profile and business forms
   const [isProfileValid, setIsProfileValid] = useState(false);
   const [isBusinessValid, setIsBusinessValid] = useState(false);
+
+  // State to manage error messages for the profile and business forms
   const [profileErrors, setProfileErrors] = useState({});
   const [businessErrors, setBusinessErrors] = useState({});
 
- const nextStep = () => {
-   // If the current step is 1
-   if (step === 1) {
-     // Validate the profile form
-     validateProfile();
-     // If the profile form is valid, proceed to the next step
-     if (isProfileValid) setStep(step + 1);
-   }
-   // If the current step is 2
-   else if (step === 2) {
-     // Validate the business form
-     validateBusiness();
-     // If the business form is valid, proceed to the next step
-     console.log("valid",isBusinessValid)
-     if (isBusinessValid) setStep(step + 1);
-   }
- };
+  // Function to proceed to the next step
+  const nextStep = () => {
+    // If the current step is 1
+    if (step === 1) {
+      // Validate the profile form
+      validateProfile();
+      // If the profile form is valid, proceed to the next step
+      if (isProfileValid) setStep(step + 1);
+    }
+    // If the current step is 2
+    else if (step === 2) {
+      // Validate the business form
+      validateBusiness();
+      // If the business form is valid, proceed to the next step
 
+      if (isBusinessValid) setStep(step + 1);
+    }
+  };
 
+  // Function to go back to the previous step
   const prevStep = () => {
     if (step > 1) setStep(step - 1);
   };
@@ -50,7 +58,7 @@ const Registration = () => {
   const handleSubmit = () => {
     if (isProfileValid && isBusinessValid) {
       // Retrieve existing data from local storage
-      console.log("valid")
+
       const existingData =
         JSON.parse(localStorage.getItem("registrationData")) || [];
 
@@ -59,7 +67,9 @@ const Registration = () => {
 
       // Save the updated data array back to local storage
       localStorage.setItem("registrationData", JSON.stringify(updatedData));
-      alert("Registration data saved successfully!");
+      alert("Registration completed successfully!");
+
+      // Reset the form to the initial state
       setStep(1);
       setFormData({
         firstName: "",
@@ -78,9 +88,11 @@ const Registration = () => {
     }
   };
 
+  // Function to validate the profile form
   const validateProfile = () => {
     const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email);
-    const isValidPhone = /^\d{10,}$/.test(formData.phone) && formData.phone.length===10 ; // Ensures the phone number is only digits and at least 10 digits long
+    const isValidPhone =
+      /^\d{10,}$/.test(formData.phone) && formData.phone.length === 10; // Ensures the phone number is only digits and at least 10 digits long
     const isValidPassword = formData.password.length > 7; // Ensures the password length is more than 7 characters
 
     setIsProfileValid(
@@ -92,12 +104,15 @@ const Registration = () => {
         formData.password === formData.confirmPassword // Ensures password matches confirmPassword
     );
 
+    // Set error messages for invalid fields
     setProfileErrors({
       firstName: !formData.firstName ? "First name is required" : "",
       lastName: !formData.lastName ? "Last name is required" : "",
       email: !isValidEmail ? "Valid email is required" : "",
       phone: !isValidPhone ? "Valid phone number is required" : "",
-      password: !isValidPassword ? "Valid password need to have minimum 8 letters" : "",
+      password: !isValidPassword
+        ? "Valid password need to have minimum 8 letters"
+        : "",
       confirmPassword:
         formData.password !== formData.confirmPassword
           ? "Passwords do not match"
@@ -105,9 +120,11 @@ const Registration = () => {
     });
   };
 
+  // Function to validate the business form
   const validateBusiness = () => {
-    const isValidZipcode = /^\d{6}$/.test(formData.zipCode) && formData.zipCode.length === 6;
- // Ensures the phone number is only digits and at least 10 digits long
+    const isValidZipcode =
+      /^\d{6}$/.test(formData.zipCode) && formData.zipCode.length === 6;
+    // Ensures the zip code is only digits and  6 digits long
     setIsBusinessValid(
       !!formData.brandName &&
         !!formData.streetAddress &&
@@ -116,13 +133,16 @@ const Registration = () => {
         !!formData.taxId
     );
 
+    // Set error messages for invalid fields
     setBusinessErrors({
       brandName: !formData.brandName ? "Brand name is required" : "",
       streetAddress: !formData.streetAddress
         ? "Street address is required"
         : "",
       city: !formData.city ? "City is required" : "",
-      zipCode: !isValidZipcode ? "Valid Zip code need minimum 6 digit required" : "",
+      zipCode: !isValidZipcode
+        ? "Valid Zip code need minimum 6 digit required"
+        : "",
       taxId: !formData.taxId ? "Tax ID number is required" : "",
     });
   };
@@ -134,9 +154,11 @@ const Registration = () => {
       </h1>
 
       <div className="w-full max-w-2xl min-w-fit mx-auto  bg-white rounded-lg shadow-lg">
+        {/* Step indicator component */}
         <StepIndicator step={step} />
         {step === 1 && (
           <>
+            {/* Profile form component */}
             <ProfileForm
               formData={formData}
               setFormData={setFormData}
@@ -162,6 +184,7 @@ const Registration = () => {
         )}
         {step === 2 && (
           <>
+            {/* Business form component */}
             <BusinessForm
               formData={formData}
               setFormData={setFormData}
@@ -187,7 +210,6 @@ const Registration = () => {
         )}
         {step === 3 && (
           <>
-            
             <div className="flex justify-between mt-6 p-8">
               <button
                 onClick={prevStep}
